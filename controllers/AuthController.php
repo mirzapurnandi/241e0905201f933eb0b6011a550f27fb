8a9bcd53b3851c3e45a463613c7c28b6c8e53916
mirzapurnandi <mirza.purnandi@gmail.com>
2023-01-31 00:53:28 +0700

    menambahkan jwt token login dan membuat function me

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -1,23 +1,30 @@
 <?php
 
+require_once 'vendor/autoload.php';
 require_once 'config/Connect.php';
 require_once 'config/MailConnect.php';
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+use Firebase\JWT\ExpiredException;
 
 class AuthController
 {
     private $conn;
+    private $key;
 
     public function __construct()
     {
         $this->conn = new Connect();
+        $this->key = 'kasdjakswer832823nkwernkwejbdskdfjsbdwesdfsd324nddf';
     }
 
     public function testing_connect()
     {
         if (!$this->conn->getConnection()) {
-            echo "Error : Unable to open database\n";
+            echo "Error : Unable to open database";
         } else {
-            echo "Opened database successfully\n";
+            echo "Opened database successfully";
         }
     }
 
@@ -25,24 +32,24 @@ class AuthController
     {
         try {
             $message = '';
-            $status = 200;
+            $status = 201;
             $result = null;
             if (!isset($data['name'])) {
-                http_response_code(401);
+                $status = 401;
+                http_response_code($status);
                 $message = 'Field Name harus diisi';
-                $status = 401;
             } elseif (!isset($data['email'])) {
-                http_response_code(401);
+                $status = 401;
+                http_response_code($status);
                 $message = 'Field Email harus diisi';
-                $status = 401;
             } elseif (!isset($data['password'])) {
-                http_response_code(401);
-                $message = 'Field Password harus diisi';
                 $status = 401;
+                http_response_code($status);
+                $message = 'Field Password harus diisi';
             }
 
-            if ($status == 200) {
-                http_response_code(201);
+            if ($status == 201) {
+                http_response_code($status);
                 $password = md5($data['password']);
                 $insert = pg_query($this->conn->getConnection(), "INSERT INTO users (name, email, password) VALUES ('" . $data['name'] . "', '" . $data['email'] . "', '" . $password . "') RETURNING *");
                 $message = 'Successfully insert data';
@@ -65,10 +72,104 @@ class AuthController
                 'result' => $result
             ]);
         } catch (\Throwable $th) {
+            http_response_code(500);
             echo json_encode([
                 'error' => true,
                 'status' => $th->getCode(),
                 'message' => $th->getMessage()
+            ]);
+        }
+    }
+
+    public function login($data)
+    {
+        try {
+            $message = '';
+            $status = 200;
+            $result = null;
+
+            if (!isset($data['email'])) {
+                $status = 400;
+                http_response_code($status);
+                $message = 'Field Email harus diisi';
+            } elseif (!isset($data['password'])) {
+                $status = 400;
+                http_response_code($status);
+                $message = 'Field Password harus diisi';
+            }
+
+            if ($status == 200) {
+                http_response_code($status);
+                $email = $data['email'];
+                $password = md5($data['password']);
+                $expired_time = time() + (1 * 60); // 15 menit
+                $date   = new DateTimeImmutable();
+
+                //check data
+                $query = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
+                $check = pg_query($this->conn->getConnection(), $query);
+                if (pg_num_rows($check) > 0) {
+                    $response = pg_fetch_array($check, 0);
+
+                    $token = [
+                        "iss" => 'http://send-mail.test',
+                        "aud" => 'http://send-mail.test',
+                        "iat" => $date->getTimestamp(),
+                        "nbf" => $date->getTimestamp(),
+                        'exp'  => $expired_time,
+                        "data" => [
+                            "username" => $email,
+                            "name" => $response['name']
+                        ]
+                    ];
+
+                    $access_token = JWT::encode($token, $this->key, 'HS256');
+
+                    $result = [
+                        'name' => $response['name'],
+                        'email' => $response['email'],
+                        'status' => $response['status']
+                    ];
+                    $message = "Login successfully";
+                } else {
+                    echo json_encode([
+                        'error' => false,
+                        'status' => 200,
+                        'message' => 'Login failed',
+                    ]);
+                    exit;
+                }
+            }
+
+            echo json_encode([
+                'error' => false,
+                'status' => $status,
+                'message' => $message,
+                'result' => $result,
+                'accessToken' => $access_token
+            ]);
+        } catch (\Throwable $th) {
+            http_response_code(500);
+            echo json_encode([
+                'error' => true,
+                'status' => $th->getCode(),
+                'message' => $th->getMessage()
+            ]);
+        }
+    }
+
+    public function me($jwt)
+    {
+        $token = explode(' ', $jwt);
+        try {
+            $decoded = JWT::decode($token[1], new Key($this->key, 'HS256'));
+            echo json_encode($decoded);
+        } catch (\Exception $th) {
+            http_response_code(401);
+            echo json_encode([
+                'error' => true,
+                'status' => 401,
+                'message' => $th->getMessage(),
             ]);
         }
     }
--- index.php
+++ index.php
@@ -1,15 +1,61 @@
 <?php
+require_once 'vendor/autoload.php';
+
 header("Access-Control-Allow-Origin: *");
 header('Content-Type: application/json');
 require_once 'controllers/AuthController.php';
+
+/* use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load(); */
 // get request method
 $method = $_SERVER['REQUEST_METHOD'];
 $uriSegments = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 
 if (isset($uriSegments[2]) && $uriSegments[2] == 'register') {
-    $controller = new AuthController();
-    $data = json_decode(file_get_contents('php://input'), true);
-    return $controller->register($data);
+    if ($method == 'POST') {
+        $controller = new AuthController();
+        $data = json_decode(file_get_contents('php://input'), true);
+        return $controller->register($data);
+    } else {
+        echo json_encode([
+            'error' => true,
+            'status' => 405,
+            'message' => 'Method harus POST'
+        ]);
+    }
+} elseif (isset($uriSegments[2]) && $uriSegments[2] == 'login') {
+    if ($method == 'POST') {
+        $controller = new AuthController();
+        $data = json_decode(file_get_contents('php://input'), true);
+        return $controller->login($data);
+    } else {
+        echo json_encode([
+            'error' => true,
+            'status' => 405,
+            'message' => 'Method harus POST'
+        ]);
+    }
+} elseif (isset($uriSegments[2]) && $uriSegments[2] == 'me') {
+    $jwt = $_SERVER['HTTP_AUTHORIZATION'];
+    if ($method == 'GET') {
+        $controller = new AuthController();
+        //$data = json_decode(file_get_contents('php://input'), true);
+        return $controller->me($jwt);
+    } elseif (!isset($jwt)) {
+        echo json_encode([
+            'error' => true,
+            'status' => 401,
+            'message' => 'Middleware Bearer Token...'
+        ]);
+    } else {
+        echo json_encode([
+            'error' => true,
+            'status' => 405,
+            'message' => 'Method harus GET'
+        ]);
+    }
 } else {
     http_response_code(404);
     echo json_encode([
